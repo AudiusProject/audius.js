73af7a3de7cfa1dc75b415c884693055f574cca3
Michael Piazza <michael.piazza.mp@gmail.com>
2020-04-28 22:56:27 -0700

    Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import external from 'rollup-plugin-peer-deps-external'
 import url from 'rollup-plugin-url'
 import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
-import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 import pkg from './package.json'
 
