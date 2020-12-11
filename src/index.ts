c597cd9738b887e495639f420aed0ef56be68e74
Michael Piazza <michael.piazza.mp@gmail.com>
2020-12-11 14:21:13 -0800

    WIP, hitting endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import btoa from 'btoa'
+import { isRight } from 'fp-ts/lib/Either'
+import {
+  getUserFollowsResource,
+  GetUserFollowsArgs,
+  GetUserFollowsOptions,
+  Resource,
+  QueryParams
+} from 'resources/GetUserFollowers'
+import fetch from 'node-fetch'
 
 // Minor hack alert:
 // `btoa` is required as a global
@@ -8,4 +17,127 @@ import btoa from 'btoa'
 // @ts-ignore
 global.btoa = btoa
 
+/**
+ * Handles selecting discovery providers, reselecting, etc
+ */
+class DiscoveryNodeManager {
+  init() {}
+  getEndpoint() {
+    // TODO: make this autoselect
+    return 'https://discoveryprovider.audius.co'
+  }
+}
+
+type Result<R, E extends Error> = (R & { error: null }) | { error: E }
+
+type Nullable<T> = T | null
+
+const success = <R, E extends Error>(result: R) => ({
+  ...result,
+  error: null as E
+})
+const error = <E extends Error>(error: E) => ({ error })
+
+const ENDPOINT_PREFIXES = {
+  full: 'v1/full',
+  regular: 'v1'
+}
+
+/**
+ * Handles making requests, retries, debouncing, etc
+ */
+class RESTClient {
+  host: string
+  currentUserId: string
+
+  constructor({
+    host,
+    currentUserId
+  }: {
+    host: string
+    currentUserId: Nullable<string>
+  }) {
+    this.host = host
+    // TODO: this needs to be nullable
+    this.currentUserId = currentUserId
+  }
+
+  async makeRequest<Args, Options, Output>(
+    args: Args,
+    options: Options,
+    resource: Resource<Args, Options, Output>
+    /* TODO - add retries here */
+  ): Promise<Result<Output, any>> {
+    const query = resource.queryBuilder(args, options, this.currentUserId)
+    const endpoint = this._constructUrl(
+      query.path,
+      query.queryParams,
+      resource.isFull
+    )
+    console.log({ endpoint })
+    const res = await fetch(endpoint)
+    const json = await res.json()
+    const decoded = resource.decoder.decode(json)
+    if (isRight(decoded)) {
+      return success(decoded.right)
+    } else {
+      console.log('Got error!')
+      console.log('Error: ' + JSON.stringify(decoded.left))
+      // TODO: handle multiple errors!
+      return error(new Error(decoded.left[0].message))
+    }
+  }
+
+  _constructUrl(path: string, params: QueryParams, isFull: boolean) {
+    const queryString = Object.entries(params)
+      .filter(p => p[1] !== undefined && p[1] !== null)
+      .map(p => {
+        if (Array.isArray(p[1])) {
+          return p[1].map(val => `${p[0]}=${encodeURIComponent(val)}`).join('&')
+        }
+        return `${p[0]}=${encodeURIComponent(p[1]!)}`
+      })
+      .join('&')
+    const prefix = isFull ? ENDPOINT_PREFIXES.full : ENDPOINT_PREFIXES.regular
+    return `${this.host}/${prefix}/${path}${
+      queryString ? `?${queryString}` : ''
+    }`
+  }
+}
+
+const userRoutes = (client: RESTClient) => ({
+  getFollowers: async (
+    args: GetUserFollowsArgs,
+    options: GetUserFollowsOptions = {}
+  ) => {
+    const resource = getUserFollowsResource
+    return client.makeRequest(args, options, resource)
+  }
+})
+
+/**
+ * Top level class, orchestrates managers responsible
+ * for performing more specific operations
+ */
+class Audius {
+  discoveryNodeManager: DiscoveryNodeManager = null
+  restClient: RESTClient = null
+  Users: ReturnType<typeof userRoutes> = null /* TODO: need to type this */
+
+  constructor() {
+    this.discoveryNodeManager = new DiscoveryNodeManager()
+    this.restClient = new RESTClient({
+      host: 'https://discoveryprovider.audius.co',
+      currentUserId: null
+    })
+    this.discoveryNodeManager.init()
+    this.Users = userRoutes(this.restClient)
+  }
+}
+
 export default Audius
+
+const a = new Audius()
+a.Users.getFollowers({ userId: 'nVvkb' }).then(res => {
+  console.log({ res })
+})
--- src/resources/GetUserFollowers.ts
+++ src/resources/GetUserFollowers.ts
@@ -1,3 +1,4 @@
+import { Either } from 'fp-ts/lib/Either'
 import * as t from 'io-ts'
 import { VersionMetadata } from '../models/Common'
 import { User } from '../models/Users'
@@ -6,7 +7,6 @@ export const FollowersResponse = t.intersection([
   t.type({
     latest_chain_block: t.number,
     latest_indexed_block: t.number,
-    owner_wallet: t.number,
     signature: t.string,
     timestamp: t.string,
     version: VersionMetadata
@@ -14,3 +14,48 @@ export const FollowersResponse = t.intersection([
   t.partial({ data: t.array(User) })
 ])
 export type FollowersResponse = t.TypeOf<typeof FollowersResponse>
+
+type Decoder<T> = {
+  decode: (i: unknown) => Either<t.Errors, T>
+}
+
+// Move this to result utils
+
+export type QueryParams = {
+  [key: string]: string | number | undefined | boolean | Array<string> | null
+}
+// TODO: need to define error types
+export type Resource<Args, Options, Output> = {
+  queryBuilder: (
+    args: Args,
+    options: Options,
+    currentUserId: string
+  ) => { path: string; queryParams: QueryParams }
+  decoder: Decoder<Output>
+  isFull: boolean
+}
+
+export type GetUserFollowsArgs = {
+  userId: string
+}
+export type GetUserFollowsOptions = {
+  offset?: number
+  limit?: number
+}
+
+export const getUserFollowsResource: Resource<
+  GetUserFollowsArgs,
+  GetUserFollowsOptions,
+  FollowersResponse
+> = {
+  decoder: FollowersResponse,
+  queryBuilder: (args, options, currentUserId) => ({
+    path: `users/${args.userId}/followers`,
+    queryParams: {
+      user_id: currentUserId,
+      limit: options.limit,
+      offset: options.offset
+    }
+  }),
+  isFull: true
+}
